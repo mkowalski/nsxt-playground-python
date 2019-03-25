a4165204998058dca9ae1026e85e66d7c6e518cc
Mateusz Kowalski <2862927+mkowalski@users.noreply.github.com>
2019-03-25 15:25:35 +0100

    Sample listing

--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
+from com.vmware.nsx_client import LogicalRouters
 from com.vmware.nsx_client import LogicalSwitches
 from vmware.vapi.bindings.struct import PrettyPrinter
 
@@ -7,8 +8,11 @@ from api_connector import create_api_connection
 def main():
     connection = create_api_connection()
 
-    logicalswitches_svc = LogicalSwitches(connection)
-    result = logicalswitches_svc.list()
+    # logicalswitches_svc = LogicalSwitches(connection)
+    # result = logicalswitches_svc.list()
+
+    logicalrouters_svc = LogicalRouters(connection)
+    result = logicalrouters_svc.list()
 
     pp = PrettyPrinter()
     pp.pprint(result)
